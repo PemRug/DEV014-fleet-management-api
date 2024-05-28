955ae116f2c85c0bb98833aa5a9bab111e402c68
PemRug <paolamejia955@gmail.com>
2024-05-27 22:06:32 -0500

    Swagger y probando hacer test

--- src/test/java/fleetmanagementapi/FleetManagementApiApplicationTests.java
+++ src/test/java/fleetmanagementapi/FleetManagementApiApplicationTests.java
@@ -1,13 +1,28 @@
 package fleetmanagementapi;
 
+import fleetmanagementapi.service.TaxisService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 @SpringBootTest
 class FleetManagementApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private TaxisService taxisService;
+
+	//@BeforeEach
+
+
+	//@Test
 
 }
